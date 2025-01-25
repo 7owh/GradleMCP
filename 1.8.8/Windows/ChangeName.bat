65f17bf7ad8f4c686a46110e5fc7f558879d9574
7owh <115172127+1Kxhu@users.noreply.github.com>
2025-01-25 18:34:13 +0100

    Add files via upload

---
+++ 1.8.8/Windows/ChangeName.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal enabledelayedexpansion
+
+echo What is the Client's name? (for example: 'MyClient v1')
+SET /P name=^> 
+
+echo '%name%' is the new name, please wait..
+
+set count=0
+:: If name is for example "Hello"
+:: the new line should be "version 'Hello'"
+set newLine=version ^'%name%^'
+set inputFile=build.gradle
+set outputFile=output.txt
+
+> %outputFile% (
+    for /f "tokens=*" %%a in (%inputFile%) do (
+        set /a count+=1
+        if !count! == 5 (
+            echo !newLine!
+        ) else (
+            echo %%a
+        )
+    )
+)
+
+:: Optionally, replace the original file with the new one
+move /y %outputFile% %inputFile%
+
+endlocal
+pause
